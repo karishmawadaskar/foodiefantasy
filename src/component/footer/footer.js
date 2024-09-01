a6d32fd4843d218862b0434de85b025990ca0164
karishma wadaskar <karishmawadaskar@gmail.com>
2024-09-01 20:14:45 +0530

    added footer

---
+++ src/component/footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.css";
+
+function Footer(){
+    return(
+        <div className="footer-text" >
+            <h1>copyright 2023 all reserved...</h1>
+        </div>
+    )
+}
+
+export default Footer

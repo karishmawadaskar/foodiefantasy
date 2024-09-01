4297e7f8ece4432350fb977cc295158ac32d3972
karishma wadaskar <karishmawadaskar@gmail.com>
2024-09-01 22:38:41 +0530

    added config UI

--- src/view/home/home.js
+++ src/view/home/home.js
@@ -3,52 +3,9 @@ import "./../../index.css"
 import FoodCard from "../../component/Foodcard/Foodcard"
 import Footer from "../../component/Footer/Footer"
 import Navbar from '../../component/Navbar/Navbar'
-
+import { PRODUCTS,MAIN_TITLE } from '../../component/config/data'
 //objects of array to fit all foodcards
-const foods = [
-  {
-    imgUrl: " https://img.freepik.com/free-photo/top-view-delicious-noodles-concept_23-2148773775.jpg?t=st=1725182186~exp=1725185786~hmac=b945b78f0726424f5a00321c016bb944a95e44f0d7e5d7fc441bab83e94063d4&w=996",
-    title: "Noodles",
-    description: " Noodles are a good source of carbohydrates, which provide energy for the body.",
-    isveg: true,
-    prize: 80
-  },
-  {
-    imgUrl: "https://img.freepik.com/free-photo/pizza-pizza-filled-with-tomatoes-salami-olives_140725-1200.jpg?t=st=1725182148~exp=1725185748~hmac=53f3a7d45d154e50d76c700f37ea73387fe52d0460bee47d89f3295b1776f4af&w=740",
-    title: "Pizza",
-    description: "The crust is thin and crispy, and the toppings are always fresh and flavorful",
-    isveg: true,
-    prize: 180
-  },
-  {
-    imgUrl: "https://img.freepik.com/free-photo/view-3d-delicious-looking-burger_23-2150914673.jpg?t=st=1725183232~exp=1725186832~hmac=35885dba50fcffdc437f478fdc2d278e014ef0cb5583629c07930d4a756f6df1&w=740",
-    title: "Burger",
-    description: "the best burgers are like life — messy and topped with bacon.",
-    isveg: true,
-    prize: 100
-  },
-  {
-    imgUrl: "https://img.freepik.com/free-photo/juicy-cheeseburger-wooden-cutting-board_9975-24304.jpg?t=st=1725183395~exp=1725186995~hmac=7b42556194337adcca6ab30c6ce3ddb240182bd0a3924e64e3145b17ce1a08c7&w=1380",
-    title: "Burger",
-    description: "the best burgers are like life — messy and topped with bacon.",
-    isveg: false,
-    prize: 150
-  },
-  {
-    imgUrl: "https://img.freepik.com/premium-photo/plate-dumplings-with-dip-sauce_1041545-48081.jpg?w=1380",
-    title: "momos",
-    description: "Momo are basically dough wrapped around stuffed vegetables or meat..",
-    isveg: true,
-    prize: 80
-  },
-  {
-    imgUrl: "https://img.freepik.com/premium-photo/gyoza-with-cranberry-horseradish-sauce-yummy-delicious-gyoza-food-image-photography_1295756-158157.jpg?w=996",
-    title: "momos",
-    description: "Momo are basically dough wrapped around stuffed vegetables or meat..",
-    isveg: false,
-    prize: 90
-  }
-]
+
 
 //run jsx
 function Home() {
@@ -56,10 +13,10 @@ function Home() {
     <div>
       <Navbar />
       <div>
-        <h1 className='heading'>Hellthy Junk Food</h1>
+        <h1 className='heading'>{MAIN_TITLE}</h1>
 
         <div className='foodcard-container'>
-          {foods.map((foodItem) => {
+          {PRODUCTS.map((foodItem) => {
             return (
               <FoodCard
                 key={foodItem.title}  // A unique key prop is important for list items
